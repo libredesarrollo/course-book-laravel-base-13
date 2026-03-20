707fc0aa4e505e63eaa4e4b4b3d11149b66b372f
andres <desarrollolibre.net@gmail.com>
2026-03-20 15:51:31 -0400

    rutas primera parte

--- routes/web.php
+++ routes/web.php
@@ -8,13 +8,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome',['name' => 'John ']);
 });
-
-// Route::get('/post', [PostController::class, 'index']);
-// Route::get('/post/create', [PostController::class, 'create']);
-// // Route::get('/post/{post}', [PostController::class, 'edit']);
-// // Route::get('/post/{post:int}', [PostController::class, 'edit']);
-// Route::get('/post/{post}', [PostController::class, 'edit']);
-// Route::get('/post/delete/{category}', [PostController::class, 'destroy']);
-
-Route::resource('post',PostController::class);
-Route::resource('category',CategoryController::class);
+http://larafirststep.test/dashboard/post/post/create
+Route::group(['prefix' => 'dashboard'], function () {
+    Route::resource('post',PostController::class);
+    Route::resource('category',CategoryController::class);
+});
